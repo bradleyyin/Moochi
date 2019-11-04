5355d8c2f9ac23dcfca208d0952866cc93207ba6
bradleyyin <bradleyyin@gmail.com>
2019-11-03 23:50:29 -0800

    use scrollview to allow zooming for receipt

--- budget/VC/reciept/ReceiptViewController.swift
+++ budget/VC/reciept/ReceiptViewController.swift
@@ -11,14 +11,20 @@ import UIKit
 class ReceiptViewController: UIViewController {
     
     var image: UIImage?
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureScrollView()
         setUpView()
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 6.0
 
         // Do any additional setup after loading the view.
     }
-    func setUpView(){
+    func setUpView() {
         
         self.view.backgroundColor = .white
         
@@ -28,23 +34,42 @@ class ReceiptViewController: UIViewController {
         button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
         button.widthAnchor.constraint(equalToConstant: buttonWidth * heightRatio).isActive = true
         button.heightAnchor.constraint(equalToConstant: buttonHeight * heightRatio).isActive = true
-        button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20 * heightRatio - buttonHeight / 2).isActive = true
+        button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20 * heightRatio).isActive = true
         button.setImage(UIImage(named: "cancel"), for: .normal)
         button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
         
+        scrollView.topAnchor.constraint(equalTo: button.bottomAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+    }
+    private func configureScrollView() {
+        
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(scrollView)
+        
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(imageView)
-        imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: button.bottomAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scrollView.addSubview(imageView)
+        imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
         imageView.image = image
         imageView.contentMode = .scaleAspectFit
+        self.imageView = imageView
+        self.scrollView = scrollView
     }
-    @objc func cancelButtonTapped(){
+    @objc func cancelButtonTapped() {
         self.dismiss(animated: true, completion: nil)
     }
 
 }
+extension ReceiptViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
 
+        return imageView
+    }
+}
